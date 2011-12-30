8368350984734b274f12166ebe5cbc0f12358f68
Bo Gou <bgou@hzwowpad.com>
2011-12-30 17:22:28 +0800

    add ssh file

--- convmv-gbk-2-uft8.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Why use this: 用unzip解压的gbk编码的文件名会出现乱码,在用unzip解压文件后再用这个命令就可以正常显示文件名.
-# Usage: convmv-gbk-2-uft8.sh GBK编码的文件名
-convmv -f GBK -t UTF-8 --notest $1
--- wifi_adb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-adb kill-server
-adb connect 192.168.11.$1:5555
-adb shell
-adb shell mount -o remount,rw system /system
-adb shell sync
-echo "Done....."
