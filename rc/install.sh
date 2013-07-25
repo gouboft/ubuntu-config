f8877536242879df8213ea6278e4a428d1cbca05
Bo Gou <link@gps-e.com>
2013-07-25 09:36:41 +0800

    add google download account

--- rc/install.sh
+++ rc/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
-for n in bashrc bash_profile inputrc kermrc gitconfig gitignore tmux.conf zshrc
+for n in bashrc bash_profile inputrc kermrc gitconfig gitignore tmux.conf zshrc netrc
 do
 	ln -sfv -T .rc/$n .$n
 done
